bf9e5563f09a659979f497986226fe867edc4959
Yennyfer Garcia <67648467+Yenny-1301@users.noreply.github.com>
2025-10-12 22:31:12 -0300

    creacion de formulario de creacion de turnos y funcionalidad de boton de agregar turno

--- app/src/main/java/com/example/gestionturnos/Turno.java
+++ app/src/main/java/com/example/gestionturnos/Turno.java
@@ -1,24 +1,33 @@
 package com.example.gestionturnos;
 
-public class Turno {
+import java.io.Serializable;
+public class Turno implements Serializable {
     private String nombreCliente;
+    private String apellidoCliente;
     private String fecha;
     private String hora;
     private String contacto;
     private String servicio;
-
-    public Turno(String nombreCliente, String fecha, String hora, String contacto, String servicio) {
+    private String comentarios;
+    private String estado;
+    public Turno(String nombreCliente, String apellidoCliente,String fecha, String hora, String contacto, String servicio, String comentarios) {
         this.nombreCliente = nombreCliente;
         this.fecha = fecha;
         this.hora = hora;
         this.contacto = contacto;
         this.servicio = servicio;
+        this.apellidoCliente = apellidoCliente;
+        this.comentarios = comentarios;
+        this.estado = "Pendiente";
     }
 
     public String getNombreCliente() { return nombreCliente; }
+    public String getApellidoCliente(){ return apellidoCliente;}
     public String getFecha() { return fecha; }
     public String getHora() { return hora; }
     public String getContacto() { return contacto; }
     public String getServicio() { return servicio; }
+    public String getComentarios(){return comentarios;}
+    public String getEstado(){return estado;}
 
 }
